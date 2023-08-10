5f7bb1a022409b9a292b276d6d36c904a211f5e2
John Quaranta <john@Johns-MacBook-Air.local>
2023-08-10 18:36:30 -0500

    Model Work (needs tweaking)

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+})
