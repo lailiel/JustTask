13e2f1833af089785db41a63af2fd037476bb79d
Abi L <124858748+lailiel@users.noreply.github.com>
2023-08-09 21:06:03 -0500

    react pages setup, nav bar set up, pages load and navbar works

---
+++ client/src/pages/GroupPage.jsx
@@ -0,0 +1,16 @@
+import Container from "react-bootstrap/Container";
+
+export default function AboutPage() {
+  return (
+    <Container fluid className="p-5" id="homepage">
+      <div className="">
+        <h2>GROUP PAGE</h2>
+      </div>
+
+      <div id='home-text'>
+        <p>THIS IS A TEST</p>
+
+      </div>
+    </Container>
+  );
+}
