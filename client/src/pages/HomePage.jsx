00187a4ba00398deb3b9546376bed96d1bf50ef7
Abi L <124858748+lailiel@users.noreply.github.com>
2023-08-17 16:32:14 -0500

    resolved conflict on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,19 +8,11 @@ export default function HomePage() {
         <h2>HOMEPAGE</h2>
       </div>
       <div id='home-text'>
-<<<<<<< HEAD
-        <p>THIS IS A PRETTY COOL APP</p>
-        <p>IT HELPS YOU GET STUFF DONE!</p>
-        <p>WITHOUT SLAPPING YOUR ROOMMATES,</p>
-        <p>YOUR SPOUSE, OR YOUR CHILDREN</p>
-        <p>JUST KIDDING....</p>
-=======
         <p>Discover the modern way to manage household responsibilities.</p>
         <p>JustTask simplifies the age-old chore chart, bringing families and housemates together in a seamless, collaborative platform.</p>
         <p>Assign tasks, track progress, and reward diligence, all in one place.</p>
         <p>No more disputes, forgotten duties, or chaotic schedules. Just tasks, made simple and fun.</p>
         <p>Start today and revolutionize how you manage chores!</p>
->>>>>>> 562cf25e3866f91643842828dfab6558f6810443
       </div>
     </Container>
   );
