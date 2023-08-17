8ce23049c4889acb1467eb3115cc96d032b50edf
Abi L <124858748+lailiel@users.noreply.github.com>
2023-08-16 19:20:38 -0500

    updated group page to only deploy tasks for given group

--- client/src/components/graphql/queries.js
+++ client/src/components/graphql/queries.js
@@ -13,6 +13,46 @@ query allUsers {
   }
 `;
 
+export const QUERY_GROUP_TASKS = gql`
+    query GroupUsers($id: ID!) {
+        group(Id: $id!) {
+            name
+            tasks{
+                id
+                taskName
+                description
+                priority
+                dueDate
+                repopulate
+                dollarValue
+                dollarAmount
+                pointValue
+                pointAmount
+                state
+                comment
+                dateOflastCompletion
+                completedBy
+            }
+    }
+}
+`
+
+export const QUERY_GROUP_MEMBERS = gql`
+    query GroupUsers($id: ID!) {
+        group(Id: $id!) {
+            name
+            owners{
+                id
+                name
+            }
+            participants{
+                id
+                name
+            }
+        }
+    }
+`;
+
 
 // export const QUERY_SINGLE_USER = gql`
 
